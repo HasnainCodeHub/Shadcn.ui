806772806b46e2ef9868a873f85556715c801c69
Hasnain Ali <hasnainbusiness204@gmail.com>
2024-09-15 03:35:18 +0500

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import { Menu } from "lucide-react";
+import Link from "next/link"
+import { Menu } from "lucide-react"
 import {
   Sheet,
   SheetContent,
@@ -57,5 +57,5 @@ export default async function Home() {
 
       </div>
     </main>
-  );
+  )
 }
